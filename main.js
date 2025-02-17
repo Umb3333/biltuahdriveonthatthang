94f79f8510860df36e7a89fb957b8e3a0cb7492b
Umb3333 <148855458+Umb3333@users.noreply.github.com>
2025-02-17 12:47:39 +0100

    S,KIVBIDI

--- main.js
+++ main.js
@@ -9,3 +9,8 @@ function autoplay () {
  videoplayer.play();
 
 }
+
+
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
